116b60efb9bf68913f0e3a858a15b1aa135363b8
Edward Wang <ed1996@gmail.com>
2019-01-07 15:57:43 -0800

    testing dynamic Dashboard

--- main.py
+++ main.py
@@ -1,13 +1,58 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial
+
+In this example, we position two push
+buttons in the bottom-right corner
+of the window.
+
+Author: Jan Bodnar
+Website: zetcode.com
+Last edited: August 2017
+"""
+
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+                             QHBoxLayout, QVBoxLayout, QApplication)
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        blueButton = QPushButton("Blue")
+        blueButton.clicked.connect(self.on_click)
+        redButton = QPushButton("Red")
+        redButton.clicked.connect(self.on_click_red)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(blueButton)
+        hbox.addWidget(redButton)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Buttons')
+        self.show()
+
+    def on_click(self):
+        self.setStyleSheet("background-color: blue;")
+
+    def on_click_red(self):
+        self.setStyleSheet("background-color: red;")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(1000, 1000)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-
+    ex = Example()
     sys.exit(app.exec_())
