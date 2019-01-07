eff3c551d707e77114edf8c275cc0869651846f1
Edward Wang <ed1996@gmail.com>
2019-01-06 19:22:12 -0800

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(1000, 1000)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
