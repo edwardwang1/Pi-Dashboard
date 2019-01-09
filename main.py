13ec75f9df27c5e4502c570bc3ac3cd48cd038a0
edward <ed1996@gmail.com>
2019-01-08 23:09:44 -0800

    working with dashboard shutdown

--- main.py
+++ main.py
@@ -16,16 +16,18 @@ Last edited: August 2017
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton,
                              QHBoxLayout, QVBoxLayout, QApplication)
+from PyQt5.QtCore import pyqtSignal                             
 import Pyro4
 import threading
 
-
 @Pyro4.expose
 class Example(QWidget):
+    shutdownSignal = pyqtSignal()
+    
 
-    def __init__(self):
+    def __init__(self, daemon):
         super().__init__()
-
+        self.daemon = daemon
         self.initUI()
 
     def initUI(self):
@@ -33,6 +35,8 @@ class Example(QWidget):
         blueButton.clicked.connect(self.change_background_to_blue)
         redButton = QPushButton("Red")
         redButton.clicked.connect(self.change_background_to_red)
+        
+        self.shutdownSignal.connect(self.closeProgram)
 
         hbox = QHBoxLayout()
         hbox.addStretch(1)
@@ -52,6 +56,8 @@ class Example(QWidget):
     def change_background_to_blue(self):
         self.setStyleSheet("background-color: blue;")
         print("test")
+        exitWhenFalse = 0
+
 
     def change_background_to_red(self):
         self.setStyleSheet("background-color: red;")
@@ -59,10 +65,20 @@ class Example(QWidget):
 
     def empty(self):
         pass
+        
+    @Pyro4.oneway
+    def shutdown(self):
+        self.daemon.shutdown()
+        self.shutdownSignal.emit()
+
+        
+    def closeProgram(self):
+        print("closing")
+        sys.exit()
 
 
 def establishingPyroServer(sharedObj):
-    daemon = Pyro4.Daemon()
+    
     uri = daemon.register(sharedObj)
     print(uri)
     #text_file = open("C:\\Users\\Edward\\Desktop\\Output.txt", "w")
@@ -70,6 +86,14 @@ def establishingPyroServer(sharedObj):
     text_file.write(str(uri))
     text_file.close()
     daemon.requestLoop()
+    daemon.close()
+
+ 
+def wipeUriFile():
+    text_file = open("uri.txt", "w")
+    text_file.write("")
+    text_file.close()
+    
 
 class PyroThread(threading.Thread):
     def __init__(self, shared, *args, **kwargs):
@@ -79,22 +103,15 @@ class PyroThread(threading.Thread):
     def run(self):
         establishingPyroServer(self.shared)
 
+import atexit
+atexit.register(wipeUriFile)
 
 
 if __name__ == '__main__':
+    daemon = Pyro4.Daemon()
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Example(daemon)
     t = PyroThread(ex)
+    t.setDaemon(True)
     t.start()
     sys.exit(app.exec_())
-
-
-
-    #text_file = open("C:\\Users\\Edward\\Desktop\\Output.txt", "w")
-    #text_file.write(str(uri))
-    #text_file.close()
-
-    #t = threading.Thread(target=daemon.requestLoop())
-    #t.start()
-    #sys.exit(app.exec_())
-    #mainloop()
