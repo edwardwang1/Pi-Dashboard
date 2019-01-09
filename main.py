9eb17899ad67473925336f3d44d58c5b67255e2d
Edward Wang <ed1996@gmail.com>
2019-01-08 18:30:54 -0800

    first test of Pyro integration

--- main.py
+++ main.py
@@ -16,8 +16,11 @@ Last edited: August 2017
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton,
                              QHBoxLayout, QVBoxLayout, QApplication)
+import Pyro4
+import threading
 
 
+@Pyro4.expose
 class Example(QWidget):
 
     def __init__(self):
@@ -48,11 +51,47 @@ class Example(QWidget):
 
     def on_click(self):
         self.setStyleSheet("background-color: blue;")
+        print("test")
 
     def on_click_red(self):
         self.setStyleSheet("background-color: red;")
+        print("Test2")
+
+
+def establishingPyroServer(sharedObj):
+    daemon = Pyro4.Daemon()
+    uri = daemon.register(sharedObj)
+    print(uri)
+    #text_file = open("C:\\Users\\Edward\\Desktop\\Output.txt", "w")
+    text_file = open("uri.txt", "w")
+    text_file.write(str(uri))
+    text_file.close()
+    daemon.requestLoop()
+
+class PyroThread(threading.Thread):
+    def __init__(self, shared, *args, **kwargs):
+        super(PyroThread, self).__init__(*args, **kwargs)
+        self.shared = shared
+
+    def run(self):
+        establishingPyroServer(self.shared)
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
+    t = PyroThread(ex)
+    t.start()
     sys.exit(app.exec_())
+
+
+
+    #text_file = open("C:\\Users\\Edward\\Desktop\\Output.txt", "w")
+    #text_file.write(str(uri))
+    #text_file.close()
+
+    #t = threading.Thread(target=daemon.requestLoop())
+    #t.start()
+    #sys.exit(app.exec_())
+    #mainloop()
